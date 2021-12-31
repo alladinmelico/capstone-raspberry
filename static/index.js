0dafcb96602a0ab69d7c9ef505f1eba9836cba5d
Alladin Melico <alladin.melico@appetiser.com.au>
2021-12-31 21:37:41 +0800

    revert index js

--- static/index.js
+++ static/index.js
@@ -1,4 +1,15 @@
-const socket = io()
-socket.on("connect", function () {
-  socket.emit("my event", { data: "I'm connected!" })
+$(document).ready(function () {
+  const profile = document.querySelector("#profile")
+  const schoolId = document.querySelector("#school-id")
+  const username = document.querySelector("#username")
+  const startTime = document.querySelector("#start-time")
+  const endTime = document.querySelector("#end-time")
+  setInterval(() => {
+    fetch("/check")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, 100)
 })
